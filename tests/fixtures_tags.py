8c6c1162108338eed59761fcc96d35dbb7b854be
smoke <smoke571reg@yandex.ru>
2020-04-18 22:21:47 +0400

    Added tag_list fixture.

--- tests/fixtures_tags.py
+++ tests/fixtures_tags.py
@@ -1,4 +1,6 @@
-__all__ = ["test_tag", "test_tag2", "test_tag3", "incorrect_tag_values"]
+from datetime import datetime, timedelta
+
+__all__ = ["test_tag", "test_tag2", "test_tag3", "incorrect_tag_values", "tag_list"]
 
 test_tag = {
     "tag_id": 1,
@@ -22,4 +24,12 @@ incorrect_tag_values = [
     ("tag_id", -1), ("tag_id", "abc"), 
     ("tag_name", 123), ("tag_name", ""), ("tag_name", "a"*256),
     ("tag_description", 1)
+]
+
+tag_list = [{
+        "tag_id": x,
+        "created_at": datetime.utcnow() + timedelta(minutes = x - 10 if x in (0, 4, 8) else x), # vowels first, consonants second
+        "tag_name": chr(ord("a") + x),
+        "tag_description": chr(ord("a") + x) + " description"
+    } for x in range(10)
 ]
