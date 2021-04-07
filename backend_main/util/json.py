866868f1b2fdd7b42dbb82a093f0bcc4f7021ac3
smoke <smoke571reg@yandex.ru>
2021-04-07 19:22:01 +0300

    row_proxy_to_dict function now sets timezone of provided objects to UTC before serializing them into strings.

--- backend_main/util/json.py
+++ backend_main/util/json.py
@@ -1,7 +1,7 @@
 """
 Data converting functions for preparing JSON responses.
 """
-from datetime import datetime
+from datetime import datetime, timezone
 import json
 
 
@@ -10,7 +10,15 @@ def row_proxy_to_dict(row_proxy):
     Converts SQLAlchemy RowProxy object into a dict.
     datetime fields are converted into strings.
     """
-    return {k: row_proxy[k] if type(row_proxy[k]) != datetime else row_proxy[k].isoformat() for k in row_proxy}
+    result = {}
+    for k in row_proxy:
+        if type(row_proxy[k]) == datetime:
+            # Add UTC timezone and serialize into string
+            date_with_timezone = row_proxy[k].replace(tzinfo=timezone.utc)
+            result[k] = date_with_timezone.isoformat()
+        else:
+            result[k] = row_proxy[k]
+    return result
 
 
 def error_json(e):
