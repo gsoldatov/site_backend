a498ecfc70e61e066a4a9d065c7b4749e8e52db7
smoke <smoke571reg@yandex.ru>
2021-09-01 13:45:11 +0300

    Added users and auth in the table schema and the Alembic migration file. Added naming conventions to SQLAlchemy table metadata. Renamed existing indexes and constraints. Updated existing migrations.

--- backend_main/db/tables.py
+++ backend_main/db/tables.py
@@ -2,13 +2,58 @@ from sqlalchemy import (
     MetaData, Table, Column, ForeignKey, Index, text,
     DateTime, Integer, String, Text, Boolean
 )
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.schema import FetchedValue
 
 def get_tables(config):
     """ Return a dictionary with SA tables and a metadata object. """
-    meta = MetaData(schema = config["db"]["db_schema"])
+    meta = MetaData(schema = config["db"]["db_schema"],
+        naming_convention={
+        "ix": "ix_%(column_0_name)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
 
     return {
+        # App settings
+        "settings": Table(
+            "settings",
+            meta,
+            Column("setting_name", String(255), primary_key=True),
+            Column("setting_value", String(255))
+        ),
+
+        # Users, sessions and log in rate limiting
+        "users": Table(
+            "users",
+            meta,
+            Column("user_id", Integer, primary_key=True, server_default=FetchedValue()),
+            Column("login", String(255), nullable=False, unique=True),
+            Column("password", Text, nullable=False),
+            Column("username", String(255), nullable=False, unique=True),
+            Column("user_level", String(16), nullable=False),
+            Column("can_edit_objects", Boolean, nullable=False)
+        ),
+
+        "sessions": Table(
+            "sessions",
+            meta,
+            Column("user_id", Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False),
+            Column("access_token", Text, nullable=False),
+            Column("expiration_time", DateTime, nullable=False)
+        ),
+
+        "login_rate_limits": Table(
+            "login_rate_limits",
+            meta,
+            Column("ip_address", postgresql.INET, primary_key=True),
+            Column("failed_login_attempts", Integer, nullable=False),
+            Column("cant_login_until", DateTime, nullable=False)
+        ),
+
+        # Objects and tags
         "tags": Table(
             "tags", 
             meta,
@@ -17,19 +62,23 @@ def get_tables(config):
             Column("modified_at", DateTime, nullable=False),
             Column("tag_name", String(255), nullable=False),
             Column("tag_description", Text),
-            Index("lowered_tag_name", text("lower(tag_name)"), unique=True)
+            Index("ix_tag_name_lowered", text("lower(tag_name)"), unique=True),
+
+            Column("is_published", Boolean, nullable=False)
         ),
     
         "objects": Table(
             "objects", 
             meta,
             Column("object_id", Integer, primary_key=True, server_default=FetchedValue()),
-            Column("object_type", String(32), nullable=False),
+            Column("object_type", String(32), nullable=False, index=True),
             Column("created_at", DateTime, nullable=False),
             Column("modified_at", DateTime, nullable=False),
             Column("object_name", String(255), nullable=False),
             Column("object_description", Text),
-            Index("object_type_index", "object_type")
+            
+            Column("owner_id", Integer, ForeignKey("users.user_id", onupdate="CASCADE", ondelete="SET NULL"), nullable=False),
+            Column("is_published", Boolean, nullable=False)
         ),
 
         "objects_tags": Table(
@@ -37,7 +86,7 @@ def get_tables(config):
             meta,
             Column("tag_id", Integer, ForeignKey("tags.tag_id", ondelete="CASCADE"), nullable=False),
             Column("object_id", Integer, ForeignKey("objects.object_id", ondelete="CASCADE"), nullable=False),
-            Index("objects_tags_index", "object_id", "tag_id", unique=True),
+            Index("ix_object_id_tag_id", "object_id", "tag_id", unique=True),
         ),
 
         "links": Table(
