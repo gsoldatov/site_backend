4b8caf36896dc3e7a21443e8162785d4eae25300
smoke <smoke571reg@yandex.ru>
2021-09-14 00:44:03 +0300

    Removed double get_config() call in main.py (in create_app & main).

--- backend_main/main.py
+++ backend_main/main.py
@@ -2,6 +2,8 @@ if __name__ == "__main__":
     import sys, os
     sys.path.insert(0, os.path.dirname(sys.path[0]))
 
+import asyncio
+
 from aiohttp import web
 from aiopg.sa import create_engine
 
@@ -17,29 +19,29 @@ from backend_main.routes import setup_routes
 
 
 async def create_app(config_file = None, config = None):
-    app = web.Application(middlewares = [error_middleware, connection_middleware, auth_middleware])
+    app = web.Application(middlewares=[error_middleware, connection_middleware, auth_middleware])
     app["config"] = config if config and type(config) == dict else get_config(config_file)
 
     db_config = app["config"]["db"]
-    app["engine"] = await create_engine(host = db_config["db_host"], 
-                                  port = db_config["db_port"], 
-                                  database = db_config["db_database"],
-                                  user = db_config["db_username"],
-                                  password = db_config["db_password"]
+    app["engine"] = await create_engine(host=db_config["db_host"], 
+                                  port=db_config["db_port"], 
+                                  database=db_config["db_database"],
+                                  user=db_config["db_username"],
+                                  password=db_config["db_password"]
                                   )
     app.on_cleanup.append(close_engine)
 
     app["tables"] = get_tables(app["config"])[0]
     setup_routes(app)
     setup_cors(app)
-
+    
     return app
 
 
 def main():
-    app = create_app()
-    config = get_config()
-    web.run_app(app, host = config["app"]["host"], port = config["app"]["port"])
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(create_app())
+    web.run_app(app, host=app["config"]["app"]["host"], port=app["config"]["app"]["port"])
 
 
 if __name__ == "__main__":
