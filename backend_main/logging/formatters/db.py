ee7e437729ea77e7c0dda84e80ec8f31ce7f9a9b
smoke <smoke571reg@yandex.ru>
2022-05-29 13:05:10 +0300

    Added MultilineFormatter class for splitting multiline messages & error information into separate log records. DB file handler now uses MultilineFormatter.

--- backend_main/logging/formatters/db.py
+++ backend_main/logging/formatters/db.py
@@ -1,8 +1,10 @@
 import logging
 
+from backend_main.logging.formatters.multiline import MultilineFormatter
+
 
 """
 Formatter for loggers in db utility.
 """
-file_db_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
+file_db_formatter = MultilineFormatter("%(asctime)s %(levelname)s %(name)s %(message)s")
 stdout_db_formatter = logging.Formatter("%(levelname)s %(name)s %(message)s")
---
+++ backend_main/logging/formatters/multiline.py
@@ -0,0 +1,36 @@
+import logging
+
+
+class MultilineFormatter(logging.Formatter):
+    """
+    Class with overriden `format` method, which produces separate LogRecord instances for each 
+    line of message and error text & stack, formats them separately and merges the resulting text lines.
+    """
+    def format(self, record):
+        # Get message text and split in by new line characters
+        lines = str(record.msg).split("\n")
+
+        # Add text lines from exception text and stack trace
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            ext_text_lines = record.exc_text.split("\n")
+            lines.extend(ext_text_lines)
+        if record.stack_info:
+            stack_info_lines = self.formatStack(record.stack_info).split("\n")
+            lines.extend(stack_info_lines)
+        
+        # Get a separate record for each line
+        records_text = []
+        for line in lines:
+            # Pass existing record params, except for `exc_info`, which was processed into `lines` items earlier
+            line_record = logging.LogRecord(record.name, record.levelno, record.pathname, record.lineno,
+                line, record.args, None, record.funcName, record.stack_info)
+            
+            records_text.append(super().format(line_record))
+        
+        # Return merged texts of line records
+        return "\n".join(records_text)
